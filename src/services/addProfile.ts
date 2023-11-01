e136506d8687f02170a23480eb9185bb95d18610
gautamaman30 <gautamaman30@gmail.com>
2023-11-01 19:23:01 +0530

    fix: add city and country while creating profile

--- src/services/addProfile.ts
+++ src/services/addProfile.ts
@@ -3,9 +3,9 @@ import { getDbClient } from '../config';
 import { interfaces, enums } from '../utils';
 
 const addProfile = async (createProfileObject: interfaces.ICreateProfileObject): Promise<interfaces.IAddProfileResponse | null> => {
-    const query = 'INSERT INTO profile(age, name, user_id, user_name) VALUES ($1, $2, $3, $4) RETURNING profile.id';
-    const { age, name, userId, userName } = createProfileObject;
-    const params = [age, name, userId, userName];
+    const query = 'INSERT INTO profile(age, name, user_id, user_name, city, country) VALUES ($1, $2, $3, $4, $5, $6) RETURNING profile.id';
+    const { age, name, userId, userName, city, country } = createProfileObject;
+    const params = [age, name, userId, userName, city, country];
     let res: QueryResult | null = null;
     const client = await getDbClient();
     try {
