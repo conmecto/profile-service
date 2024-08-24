81df2bafbcf35ac5024329a6f2c60ac06a248d38
conmecto <conmecto@gmail.com>
2024-08-24 18:03:43 +0530

    fix: removed post on profile delete

--- src/services/removeProfile.ts
+++ src/services/removeProfile.ts
@@ -3,18 +3,27 @@ import moment from 'moment';
 import { getDbClient } from '../config';
 
 const removeProfile = async (userId: number) => {
-    const query = `
+    const query1 = `
         UPDATE profile 
         SET deleted_at=$2 
         WHERE user_id=$1 AND deleted_at IS NULL
         RETURNING profile.id
     `;
+    const query2 = `
+        UPDATE post 
+        SET deleted_at=$2 
+        WHERE user_id=$1 AND deleted_at IS NULL
+    `;
     let res: QueryResult | null = null;
     const client = await getDbClient();
     const params = [userId, moment().toISOString(true)];
     try {
-        res = await client.query(query, params);
+        await client.query('BEGIN');
+        res = await client.query(query1, params);
+        await client.query(query2, params);
+        await client.query('COMMIT');
     } catch (err) {
+        await client.query('ROLLBACK')
         throw err;
     } finally {
         client.release();
